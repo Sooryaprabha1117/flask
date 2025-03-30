4c866e82e6d0c91790a8211b27b8c4b98bfb5a44
Soorya Prabha M <146364071+Sooryaprabha1117@users.noreply.github.com>
2025-03-30 19:18:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
- from flask import Flask, render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
-    return render_template("index.html")  # Loads HTML page
+    return "Hello, Flask is running on Render!"
 
 if __name__ == "__main__":
     app.run(debug=True)
+
 
